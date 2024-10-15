ceadd39d7c4b80f6e05f1c43b3c1df7d93960834
JoseDEV799 <josedev799@gmail.com>
2024-10-14 23:59:56 -0500

    Register

--- api/db.js
+++ api/db.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose'
+import { config } from 'dotenv'
+config()
+
 export const conectDB = async () => {
     try {
-        // await mongoose.connect(process.env.DATABASE_URL)
-        await mongoose.connect("mongodb://localhost:27017/cptln")
+        await mongoose.connect(process.env.DATABASE_URL)
+        await mongoose.connect()
         console.log('Conectado a la base de datos')
     } catch (error) {
         console.log(error)
